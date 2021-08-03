4da884b98e5084345f4ef6b43c6bf3c302836dc0
JRuizBurgos <julian.ruizburgos@itility.com>
2021-08-03 18:08:32 +0200

    indicators complete

--- test/tests_resolvers.py
+++ test/tests_resolvers.py
@@ -2,4 +2,18 @@ import pytest
 from graphqlserver import resolvers
 
 def test_resolve_indicator_by_id():
-    result = resolvers.resolve_indicator_by_id(None, None, 106483006625399508799825370466766658890)
+
+    expected = {
+        'id': 247952362168809575136085981609942424906, 
+        'method': 'cumulative energy demand',
+        'category': 'fossil', 
+        'indicator': 'non-renewable energy resources, fossil', 
+        'units': 'MJ-Eq'
+        }
+        
+    result = resolvers.resolve_indicator_by_id(None, None, 247952362168809575136085981609942424906)
+
+    assert expected == result
+
+def test_resolve_indicators():
+    result = resolvers.resolve_indicators(None, None)
