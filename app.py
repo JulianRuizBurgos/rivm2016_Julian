125e1847050bbbf0a4d009b5d31ac77ca6a930a7
JRuizBurgos <julian.ruizburgos@itility.com>
2021-08-03 20:33:50 +0200

    added resolvers for entry and impact.

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from ariadne import gql, graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType
 from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, request, jsonify
-
+from graphqlserver import resolvers
 from loggerwrapper import Logger
 
 app = Flask(__name__)
+
+
+type_defs = load_schema_from_path("schemas/schema.gql")
+query = QueryType()
+
+indicator = ObjectType('Indicator')
+entry = ObjectType('Entry')
+impact = ObjectType('Impact')
+geography = ObjectType('Geography')
+
+query.set_field('indicator', resolvers.resolve_indicator_by_id)
+query.set_field('indicators', resolvers.resolve_indicators)
+query.set_field('entry', resolvers.resolve_entry_by_id)
+query.set_field('entries', resolvers.resolve_entries)
+query.set_field('impact', resolvers.resolve_impact)
+
+
+schema = make_executable_schema(type_defs, [indicator, entry, impact, geography, query])
 
 @app.route('/graphql', methods=['GET'])
 def playground():
--- test/tests_resolvers.py
+++ test/tests_resolvers.py
@@ -17,3 +17,29 @@ def test_resolve_indicator_by_id():
 
 def test_resolve_indicators():
     result = resolvers.resolve_indicators(None, None)
+
+def test_resolve_entry_by_id():
+
+    expected = {
+        "id": 273737595570398359382690998602960313674,
+        "product_name": "Worst",
+        "geography": "rook/rund-",
+        "unit": "kg"
+    }
+
+    result = resolvers.resolve_entry_by_id(None, None, 273737595570398359382690998602960313674)
+
+    assert expected == result
+
+def test_resolve_entries():
+    result = resolvers.resolve_entries(None, None)
+
+def test_resolve_impact():
+
+    expected = {
+        "coefficient" : 0.399
+    }
+
+    result = resolvers.resolve_impact(None, None, 273737595570398359382690998602960313674, 273737595174257546810686733816717221194)
+
+    assert result["coefficient"] == expected["coefficient"]
